57d4a1b6df1b7d15050fac1788bd8555b613df89
Mjic99 <menyeath@hotmail.com>
2019-09-19 02:14:47 -0500

    firebase hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAQ3pDCXlxqXoiKx6p7dqljNLtOoPpIEkg",
+    authDomain: "contacta-d4362.firebaseapp.com",
+    databaseURL: "https://contacta-d4362.firebaseio.com",
+    projectId: "contacta-d4362",
+    storageBucket: "",
+    messagingSenderId: "16894724472",
+    appId: "1:16894724472:web:c346a7c3abf7e7610ca08d"
+  }
 };
